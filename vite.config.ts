15c5d4656b7f4973ca5ab60810fe936a416adec4
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2022-10-24 19:17:37 -0300

    proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,14 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://payments2.crank-fit.com",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
