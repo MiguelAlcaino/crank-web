d04f2baa0029cb31cc2687d3c06217f81f01c4d2
Miguel Alcaino <miguel.alcaino@ogb.cl>
2023-03-09 16:23:04 -0300

    Remove SpotPosition file to be replaced by an interface within the component

--- src/components/SpotMatrix.vue
+++ src/components/SpotMatrix.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 
 import {onMounted, ref} from "vue";
-import {SpotPosition} from "@/model/SpotPosition";
 import BookableSpotPosition from "@/components/BookableSpotPosition.vue"
 import IconPositionNotBookable from "@/components/icons/IconPositionNotBookable.vue"
+
+interface SpotPosition {
+  positionType: string
+  positionIcon: string
+  spotInfo?: SpotInfo
+}
 
 interface ClassPositionInterface {
   __typename: string
@@ -29,8 +34,8 @@ interface BookableSpot extends ClassPositionInterface {
 interface IconPosition extends ClassPositionInterface {
 }
 
-const bookableSpotKey = 'BookableSpot';
-const iconPositionKey = 'IconPosition';
+const BOOKABLE_SPOT_KEY = 'BookableSpot';
+const ICON_POSITION_KEY = 'IconPosition';
 
 const props = defineProps<{
   matrix: Array<ClassPositionInterface> | undefined
@@ -46,6 +51,10 @@ onMounted(() => {
   getSpotTable();
 });
 
+function newSpotPosition(positionType: string, positionIcon: string, spotInfo?: SpotInfo): SpotPosition {
+  return {positionType: positionType, positionIcon: positionIcon, spotInfo: spotInfo}
+}
+
 function getSpotTable() {
   let x = 0;
 
@@ -56,32 +65,32 @@ function getSpotTable() {
 
       let typename: string | undefined = props.matrix[i]["__typename"];
 
-      if (typename === bookableSpotKey) {
+      if (typename === BOOKABLE_SPOT_KEY) {
         let classPosition = props.matrix[i] as BookableSpot;
 
         if (x == classPosition.x) {
-          spotsTableRow.push(new SpotPosition(bookableSpotKey, classPosition.icon, classPosition.spotInfo));
+          spotsTableRow.push(newSpotPosition(BOOKABLE_SPOT_KEY, classPosition.icon, classPosition.spotInfo))
         } else {
           spotsTable.value.push(spotsTableRow);
 
           spotsTableRow = [];
           x = classPosition.x;
 
-          spotsTableRow.push(new SpotPosition(bookableSpotKey, classPosition.icon, classPosition.spotInfo));
+          spotsTableRow.push(newSpotPosition(BOOKABLE_SPOT_KEY, classPosition.icon, classPosition.spotInfo));
         }
       }
-      if (typename === iconPositionKey) {
+      if (typename === ICON_POSITION_KEY) {
         let classPosition = props.matrix[i] as IconPosition;
 
         if (x == classPosition.x) {
-          spotsTableRow.push(new SpotPosition(iconPositionKey, classPosition.icon));
+          spotsTableRow.push(newSpotPosition(ICON_POSITION_KEY, classPosition.icon));
         } else {
           spotsTable.value.push(spotsTableRow);
 
           spotsTableRow = [];
           x = classPosition.x;
 
-          spotsTableRow.push(new SpotPosition(iconPositionKey, classPosition.icon));
+          spotsTableRow.push(newSpotPosition(ICON_POSITION_KEY, classPosition.icon));
         }
       }
     }
@@ -104,12 +113,12 @@ function onClickSpotBtn(spotNumber: number) {
       <tbody>
       <tr v-for="(colRow, key) in spotsTable" :key="key">
         <td v-for="(spot, key) in colRow" :key="key">
-          <BookableSpotPosition v-if="spot.positionType === bookableSpotKey"
+          <BookableSpotPosition v-if="spot.positionType === BOOKABLE_SPOT_KEY"
                                 :spotInfo="spot.spotInfo"
                                 @click-spot="onClickSpotBtn">
           </BookableSpotPosition>
 
-          <IconPositionNotBookable v-else-if="spot.positionType === iconPositionKey" :iconName="spot.positionIcon">
+          <IconPositionNotBookable v-else-if="spot.positionType === ICON_POSITION_KEY" :iconName="spot.positionIcon">
           </IconPositionNotBookable>
         </td>
       </tr>
