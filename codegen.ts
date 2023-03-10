a7a88d49ffbbfc32395a44fcd7cfacb714038122
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2023-03-10 14:22:54 -0300

    Update codegen.ts

--- codegen.ts
+++ codegen.ts
@@ -3,11 +3,12 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   overwrite: true,
   schema: "https://payments2.crank-fit.com/api/graphql/",
-  documents: "src/**/*.vue",
+  documents: ["src/**/*.vue", "src/**/*.ts"],
   generates: {
     "./src/gql/": {
       preset: "client",
       plugins: [],
+      config: { nonOptionalTypename: true },
     },
     "./graphql.schema.json": {
       plugins: ["introspection"],
