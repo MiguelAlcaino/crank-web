0e8eafd93f9f3d8bc8266a72f14ebfd5b67fba49
Miguel Alcaino <miguel.alcaino@ogb.cl>
2023-04-04 18:14:38 -0400

    Add refresh token logic when the page is refreshed and 1 minute before the token expires

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,42 @@
-import { createApp, h, provide } from "vue";
-import { createPinia } from "pinia";
-import { ApolloClient, InMemoryCache } from "@apollo/client/core";
-import { DefaultApolloClient } from "@vue/apollo-composable";
-import { createVfm } from "vue-final-modal";
-
+import {createApp, h, provide} from "vue";
+import {createPinia} from "pinia";
+import {ApolloClient, InMemoryCache} from "@apollo/client/core";
+import {DefaultApolloClient} from "@vue/apollo-composable";
+import {createVfm} from "vue-final-modal";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import dayjs from "dayjs";
-
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "vue-final-modal/style.css";
+import {authService} from "@/services/authService";
 
-dayjs.Ls.en.weekStart = 1;
+startApp();
 
-const apolloClient = new ApolloClient({
-  uri: "https://payments2.crank-fit.com/api/graphql/",
-  cache: new InMemoryCache(),
-});
+async function startApp() {
+    dayjs.Ls.en.weekStart = 1;
 
-const app = createApp({
-  setup() {
-    provide(DefaultApolloClient, apolloClient);
-  },
-  render: () => h(App),
-});
+    const apolloClient = new ApolloClient({
+        uri: "https://payments2.crank-fit.com/api/graphql/",
+        cache: new InMemoryCache(),
+    });
 
-app.use(createPinia());
-app.use(router);
-app.config.globalProperties.$dayjs = dayjs;
+    const app = createApp({
+        setup() {
+            provide(DefaultApolloClient, apolloClient);
+        },
+        render: () => h(App),
+    });
 
-const vfm = createVfm();
-app.use(vfm);
+    app.use(createPinia());
+    app.use(router);
+    app.config.globalProperties.$dayjs = dayjs;
 
-app.mount("#app");
+    const vfm = createVfm();
+    app.use(vfm);
+
+    await authService.refreshToken()
+
+    app.mount("#app");
+}
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,32 +1,47 @@
 import axios from "axios";
-import { useAuthenticationStore } from "@/stores/authToken";
-import { IncorrectCredentialsLoginError } from "@/model/Exception";
+import {useAuthenticationStore} from "@/stores/authToken";
+import {IncorrectCredentialsLoginError} from "@/model/Exception";
 import router from "@/router";
+import jwt_decode from 'jwt-decode';
+
+interface JwtTokenPayload {
+    exp: number
+}
 
 export const authService = {
-  isLoggedId(): boolean {
-    return useAuthenticationStore().token !== null;
-  },
-  async login(email: string, password: string, site: string) {
-    try {
-      const response = await axios.post("/api/login_check?site=" + site, {
-        username: email,
-        password: password,
-      });
+    isLoggedId(): boolean {
+        return useAuthenticationStore().token !== null;
+    },
+    async login(email: string, password: string, site: string) {
+        try {
+            const response = await axios.post("/api/login_check?site=" + site, {
+                username: email,
+                password: password,
+            });
 
-      if (response.data.token) {
-        useAuthenticationStore().setSession(
-          response.data.token,
-          response.data.refresh_token
-        );
-      }
-    } catch (error) {
-      console.log(error);
-      throw new IncorrectCredentialsLoginError();
-    }
-  },
-  async logout() {
-    useAuthenticationStore().deleteSession();
-    await router.push({ name: "login" });
-  },
+            const token = response.data.token;
+            if (token) {
+                useAuthenticationStore().setSession(token);
+                this.startRefreshTokenTimer()
+            }
+        } catch (error) {
+            console.log(error);
+            throw new IncorrectCredentialsLoginError();
+        }
+    },
+    startRefreshTokenTimer() {
+        let decoded = jwt_decode<JwtTokenPayload>(useAuthenticationStore().token!);
+
+        const expires = new Date(decoded.exp * 1000);
+        const timeout = expires.getTime() - Date.now() - (60 * 1000);
+        useAuthenticationStore().setRefreshTokenTimeout(setTimeout(this.refreshToken, timeout));
+    },
+    async refreshToken() {
+        let response = await axios.post('/api/token/refresh');
+        useAuthenticationStore().setSession(response.data.token);
+    },
+    async logout() {
+        useAuthenticationStore().deleteSession();
+        await router.push({name: "login"});
+    },
 };
