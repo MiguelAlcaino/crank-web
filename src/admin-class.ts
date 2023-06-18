1accd8c3db648b9b0da99c9d12f8bec811f32a3f
Miguel Alcaino <miguel.alcaino@ogb.cl>
2023-06-18 22:43:35 +0200

    Add a second entrypoint to have admin class

---
+++ src/admin-class.ts
@@ -0,0 +1,32 @@
+import {createApp, h, provide} from 'vue'
+import {createPinia} from 'pinia'
+import {createVfm} from 'vue-final-modal'
+import AdminClass from '@/views/admin/AdminClass.vue'
+import router from '@/router'
+import '@/assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'vue-final-modal/style.css'
+import {ApiService} from '@/services/apiService'
+import {apiClient, authApiClient} from '@/services/graphqlClient'
+import {useAuthenticationStore} from '@/stores/authToken'
+
+startApp()
+
+async function startApp() {
+    const selection = <HTMLElement | null>document.querySelector('#vue-app-admin-class');
+    let mindbodyClass = JSON.parse(selection?.dataset.mindbodyClass as string)
+    let token = selection?.dataset.token as string
+    const app = createApp({
+        setup() {
+            provide('mindbodyClass', mindbodyClass)
+            provide('gqlApiService', new ApiService(authApiClient, apiClient))
+        },
+        render: () => h(AdminClass)
+    })
+
+    app.use(createPinia()).use(router).use(createVfm())
+    useAuthenticationStore().setSession(token)
+
+    app.mount('#app')
+}
