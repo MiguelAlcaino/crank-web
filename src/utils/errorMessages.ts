7a787a07b5506e75a76c58a964b8aba14d1acabe
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2023-08-30 10:39:33 -0400

    Create errorMessages.ts

---
+++ src/utils/errorMessages.ts
@@ -0,0 +1,8 @@
+// errorMessages.ts
+
+export const ERROR_UNKNOWN =
+  "UPS! SORRY, WE DIDN'T SEE THAT COMING!. PLEASE TRY AGAIN OR COMMUNICATE WITH THE TEAM TO RESOLVE THIS ISSUE."
+export const ERROR_SPOT_NOT_FOUND =
+  'The spot was not found in the list of disabled spots. This error is very unlikely to happen.'
+export const ERROR_LATECANCELLATION_REQUIRED =
+  'YOU ARE OUTSIDE THE EARLY CANCELLATION WINDOW. YOU CAN ONLY MAKE A LATE CANCELLATION.'
