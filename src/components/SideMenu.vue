02840a83933d791ec913113bc0cac27118fba76f
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2023-09-13 20:39:42 -0300

    Fixed routing in SideMenu.vue

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -12,53 +12,48 @@ const route = useRoute()
         <a class="list-group-item border-0" href="#" role="tab" @click="authService.logout()">
           <i class="bi bi-box-arrow-right" aria-hidden="true"></i> &nbsp; Log Out
         </a>
-        <a
+        <router-link
+          :to="{ name: 'workout_stats' }"
           class="list-group-item border-0"
           :class="{ active: route.name === 'workout_stats' }"
-          href="/workout-stats"
-          role="tab"
         >
-          <i class="bi bi-bar-chart-fill" aria-hidden="true"></i> &nbsp; Workout Stats
-        </a>
-        <a
+          <i class="bi bi-bar-chart-fill" aria-hidden="true"></i> &nbsp; Workout Stats</router-link
+        >
+        <router-link
+          :to="{ name: 'calendar' }"
           class="list-group-item border-0"
           :class="{ active: route.name === 'calendar' }"
-          href="/calendar"
-          role="tab"
         >
-          <i class="bi bi-calendar3" aria-hidden="true"></i> &nbsp; Book Classes
-        </a>
-        <a
+          <i class="bi bi-calendar3" aria-hidden="true"></i> &nbsp; Book Classes</router-link
+        >
+        <router-link
+          :to="{ name: 'bookings' }"
           class="list-group-item border-0"
           :class="{ active: route.name === 'bookings' }"
-          href="/bookings"
-          role="tab"
         >
-          <i class="bi bi-calendar-check" aria-hidden="true"></i> &nbsp; Bookings
-        </a>
-        <a
+          <i class="bi bi-calendar-check" aria-hidden="true"></i> &nbsp; Bookings</router-link
+        >
+        <router-link
+          :to="{ name: 'purchases' }"
           class="list-group-item border-0"
           :class="{ active: route.name === 'purchases' }"
-          href="/purchases"
-          role="tab"
         >
-          <i class="bi bi-credit-card" aria-hidden="true"></i> &nbsp; Purchases
-        </a>
-        <a
+          <i class="bi bi-credit-card" aria-hidden="true"></i> &nbsp; Purchases</router-link
+        >
+
+        <router-link
+          :to="{ name: 'profile' }"
           class="list-group-item border-0"
           :class="{ active: route.name === 'profile' }"
-          href="/profile"
-          role="tab"
         >
-          <i class="bi bi-person-fill" aria-hidden="true"></i> &nbsp; Profile
-        </a>
+          <i class="bi bi-person-fill" aria-hidden="true"></i> &nbsp; Profile</router-link
+        >
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.list-group a.active,
 .list-group a.active {
   background-color: #000000;
   border-color: #000000;
@@ -66,16 +61,12 @@ const route = useRoute()
 
 a.list-group-item:hover,
 button.list-group-item:hover,
-a.list-group-item:focus,
 button.list-group-item:focus {
   color: #757575;
   background-color: #eee;
 }
 
-a.list-group-item:focus,
-a.list-group-item:hover,
-button.list-group-item:focus,
-button.list-group-item:hover {
+a.list-group-item:hover {
   color: #757575;
   text-decoration: none;
   background-color: #eee;
