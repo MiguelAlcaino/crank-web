c89c5526c4ac67326c4cddbe30d0c88afb1cde73
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2023-09-25 11:18:56 -0300

    Create RoomLayoutListView.vue

---
+++ src/views/admin/RoomLayoutListView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { inject, onMounted } from 'vue'
+
+import DefaultButtonComponent from '@/components/DefaultButtonComponent.vue'
+import type { ApiService } from '@/services/apiService'
+import { appStore } from '@/stores/appStorage'
+
+const apiService = inject<ApiService>('gqlApiService')!
+
+onMounted(() => {
+  // TODO: get layout list
+})
+</script>
+
+<template>
+  <h1>Room Layouts</h1>
+</template>
+
+<style scoped></style>
