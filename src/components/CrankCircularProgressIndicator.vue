1010155a73882e07c0ffa28dd10a213d0efd3773
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2023-10-31 18:55:10 -0300

    Create CrankCircularProgressIndicator.vue

---
+++ src/components/CrankCircularProgressIndicator.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="spinner-border crankSpiner" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.crankSpiner {
+    color: #ff6f60;
+}
+</style>
