2989b3245c324609c49149a469fd2ed6ebee0685
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2023-12-28 18:54:39 -0300

    Payment url is added to the environment variables

--- src/views/PaymentsIframeView.vue
+++ src/views/PaymentsIframeView.vue
@@ -2,26 +2,33 @@
 import { appStore } from '@/stores/appStorage'
 import { useAuthenticationStore } from '@/stores/authToken'
 import { onMounted, ref } from 'vue'
+const paymenttUrl = (import.meta.env.VITE_PAYMENT_URL ?? '') as string | null
+
+const iframeLink = ref<string | null>(null)
 
 onMounted(() => {
   const site = appStore().site
   const token = useAuthenticationStore().token
 
   iframeLink.value =
-    'https://payments2.crank-fit.com/autologin?packages=1&site=' +
-    site +
-    '&bearer=' +
-    token +
-    '&from-mobile-app=1'
+    paymenttUrl + '/autologin?packages=1&site=' + site + '&bearer=' + token + '&from-mobile-app=1'
 })
-
-const iframeLink = ref<string | null>(null)
 </script>
 
 <template>
-  <div class="embed-responsive embed-responsive-16by9">
-    <iframe v-if="iframeLink" width="800" height="230" :src="iframeLink"></iframe>
+  <div class="container-fluid h-100">
+    <div class="row justify-content-center vh-100">
+      <div class="col-12">
+        <iframe v-if="iframeLink" :src="iframeLink"></iframe>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+iframe {
+  border: none;
+  width: 100%;
+  height: 100%;
+}
+</style>
