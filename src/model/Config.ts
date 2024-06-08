4a5e6a0ed08f50334c1e80eccff4247fe06224da
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2024-06-08 19:47:41 -0400

    graphql url is changed

--- src/model/Config.ts
+++ src/model/Config.ts
@@ -1,5 +1,5 @@
 export class Config {
-  static readonly AUTH_SERVICE_HOST = 'https://api.crank-fit.com'
+  static readonly AUTH_SERVICE_HOST = 'https://payments2.crank-fit.com'
   static readonly GRAPHQL_SERVICE_PATH = '/api/graphql/'
-  static readonly GRAPHQL_SERVICE_URL = 'https://api.crank-fit.com' + Config.GRAPHQL_SERVICE_PATH
+  static readonly GRAPHQL_SERVICE_URL = 'https://payments2.crank-fit.com' + Config.GRAPHQL_SERVICE_PATH
 }
