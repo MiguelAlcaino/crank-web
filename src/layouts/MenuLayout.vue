e32f3488883fe480be01443af1a890ff900a5f03
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2024-06-08 23:44:06 -0400

    SideMenu is removed

--- src/layouts/MenuLayout.vue
+++ src/layouts/MenuLayout.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-import SideMenu from '@/components/SideMenu.vue'
+// import SideMenu from '@/components/SideMenu.vue'
 </script>
 
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-lg-3 col-md-3 col-sm-4">
+      <!--<div class="col-lg-3 col-md-3 col-sm-4">
         <side-menu></side-menu>
       </div>
-      <div class="col-lg-9 col-md-9 col-sm-8">
+      <div class="col-lg-9 col-md-9 col-sm-8"> -->
+      <div class="col-lg-12 col-md-12 col-sm-12">
         <div class="card">
           <div class="card-body">
             <router-view></router-view>
