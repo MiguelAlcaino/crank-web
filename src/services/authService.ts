53e6bc8ee983896390ecabee59be8fc556273f21
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2024-06-14 13:26:05 -0400

    fix error token expired iframe

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -16,6 +16,11 @@ interface JwtTokenPayload {
 export const authService = {
   isLoggedId(): boolean {
     const store = useAuthenticationStore()
+
+    if(store.token !== null && this.isTokenExpired(store.token)){
+      useAuthenticationStore().deleteSession()
+    }
+
     return store.token !== null
   },
   async login(email: string, password: string, site: string): Promise<void> {
@@ -105,5 +110,21 @@ export const authService = {
     }
 
     return false
+  },
+  isTokenExpired(token: string) {
+    const base64Url = token.split('.')[1]
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+        })
+        .join('')
+    )
+  
+    const { exp } = JSON.parse(jsonPayload)
+    const expired = Date.now() >= exp * 1000
+    return expired
   }
 }
