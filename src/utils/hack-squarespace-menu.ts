a76f09393a48ae8ddc2859e69497ec1f2dc89a0d
Gabriel Correa Ramírez <26012694+Gaboae86@users.noreply.github.com>
2024-07-22 12:47:08 -0400

    logout link menu mobile

--- src/utils/hack-squarespace-menu.ts
+++ src/utils/hack-squarespace-menu.ts
@@ -195,6 +195,14 @@ function hackMobileMenu(userIsLoggedIn: boolean, crankMenuUrls: MenuUrls) {
       subMenuProfileLink.href = crankMenuUrls.profileUrl
       subMenuProfileLink.innerText = 'Profile'
       myAccountDiv.appendChild(subMenuProfileLink)
+
+      //Logout link
+      const logoutLink = document.createElement('a')
+      logoutLink.className = mobileClassName
+      logoutLink.href = '#'
+      logoutLink.innerText = 'Logout'
+      logoutLink.onclick = () => logout(crankMenuUrls?.homeUrl ?? '/')
+      myAccountDiv.appendChild(logoutLink)
     } else {
       // create New to CRANK sub menu link
       const subMenuNewToCrankLink = document.createElement('a')
