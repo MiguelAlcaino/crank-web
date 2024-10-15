ac15424b1844d97ab22cef573eacd812079860f2
Gabriel Correa Ramírez <26012694+Gaboae86@users.noreply.github.com>
2024-10-15 13:36:37 -0300

    Create is-sms-validation-code-valid-response.ts

---
+++ src/modules/buy_packages/models/is-sms-validation-code-valid-response.ts
@@ -0,0 +1,39 @@
+import { ERROR_UNKNOWN } from '@/utils/errorMessages'
+
+export class IsSmsValidationCodeValidResponse {
+  errorCode:
+    | 'MobilePhoneAlreadyVerifiedError'
+    | 'RequestSMSValidationNeededError'
+    | 'SMSCodeValidatedSuccessfully'
+    | 'SMSValidationCodeError'
+    | 'UnknownError'
+  constructor(
+    typeName:
+      | 'MobilePhoneAlreadyVerifiedError'
+      | 'RequestSMSValidationNeededError'
+      | 'SMSCodeValidatedSuccessfully'
+      | 'SMSValidationCodeError'
+      | 'UnknownError'
+  ) {
+    this.errorCode = typeName
+  }
+
+  get message(): string {
+    switch (this.errorCode) {
+      case 'SMSCodeValidatedSuccessfully':
+        return 'Thank you. Your mobile number has been verified and updated successfully and you can proceed with your purchase now.'
+      case 'SMSValidationCodeError':
+        return 'Invalid code.'
+      case 'MobilePhoneAlreadyVerifiedError':
+        return 'Mobile phone number is already validated.'
+      case 'RequestSMSValidationNeededError':
+        return 'SMS Validation is required.'
+      default:
+        return ERROR_UNKNOWN
+    }
+  }
+
+  get success(): boolean {
+    return this.errorCode === 'SMSCodeValidatedSuccessfully'
+  }
+}
