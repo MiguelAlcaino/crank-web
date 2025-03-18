abfa33028d33ce375c6771961913e3397e2cfc2b
Gabriel Correa Ramírez <gabriel.correa.r@hotmail.com>
2025-03-18 12:17:25 -0300

    Create ShoppingCartIcon.vue

---
+++ src/modules/shop/components/ShoppingCartIcon.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+defineProps<{
+  totalItems: number
+}>()
+
+const emits = defineEmits<{
+  (e: 'open'): void
+}>()
+
+const open = () => {
+  emits('open')
+}
+</script>
+
+<template>
+  <div class="icon-wrapper" @click="open">
+    <i class="bi bi-bag" style="font-size: 2rem"></i>
+    <span class="number">5</span>
+  </div>
+</template>
+
+<style lang="css" scoped src="bootstrap/dist/css/bootstrap.min.css"></style>
+<style lang="css" scoped src="@/assets/main.css"></style>
+
+<style scoped>
+.icon-wrapper {
+  cursor: pointer;
+  position: relative;
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  line-height: 50px;
+  font-size: 24px;
+  background-color: transparent;
+  border-radius: 50%;
+}
+
+.icon-wrapper .number {
+  position: absolute;
+  top: 55%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: black;
+  font-size: 10px;
+}
+</style>
